3fa14ad912ee61262ad5b200dce413be42570107
Alona-Liubchenko <aylin888@rambler.ru>
2022-12-25 23:12:34 +0200

    p3

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, ImageBackground, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
+import RegistrationScreen from './screens/RegistrationScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js  app!</Text>
-      <StatusBar style="auto" />
+      <RegistrationScreen/>
+      {/* <StatusBar style="auto" /> */}
+     
     </View>
   );
 }
@@ -14,7 +16,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+//   forma: {
+//     backgroundColor: '#FFFFFF',
+//     borderRadius: 25,
+//     // width: 375,
+//     height:549,
+// },
+//   image: {
+//     flex: 1,
+//     resizeMode: "cover",
+//     justifyContent: "flex-end",
+//   },
+//   input: {
+//     textAlign: 'left',
+//     backgroundColor: "#F6F6F6",
+//     borderColor: "#E8E8E8",
+//     borderRadius: 8,
+//      height: 40,
+//     margin: 12,
+//     borderWidth: 1,
+//     paddingLeft: 16,
+//   },
+//   btn: {
+//     backgroundColor: "#FF6C00",
+//     height:51,
+//     borderRadius:100,
+//     marginTop:43,
+//     justifyContent:"center",
+//     alignItems:"center",
+//   },
+//   btnTitle:{
+//     color: "#FFFFFF",
+//     fontSize:16,}
+},);
+
+  
---
+++ screens/RegistrationScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View, ImageBackground, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
+export default function RegistrationScreen(){
+    return(<Viev><ImageBackground style={styles.image} source={require('./assets/images/photoBG.jpg')}>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : ""}>
+    <View style={styles.forma}>
+      <Text>Registration</Text>
+      <TextInput style={ styles.input} placeholder="Login"/>
+      <TextInput style={ styles.input} placeholder="Email"/>
+      <TextInput style={ styles.input} placeholder="Password" secureTextEntry={true}/>
+      <View>
+        <TouchableOpacity style={styles.btn}><Text style={styles.btnTitle}>SING UP</Text></TouchableOpacity>
+      </View>
+    </View>
+    </KeyboardAvoidingView>
+  </ImageBackground></Viev>)
+}
+
+const styles = StyleSheet.create({
+    // container: {
+    //   flex: 1,
+    //   backgroundColor: '#fff',
+    //   // alignItems: 'center',
+    //   // justifyContent: 'center',
+    // },
+    forma: {
+      backgroundColor: '#FFFFFF',
+      borderRadius: 25,
+      // width: 375,
+      height:549,
+  },
+    image: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "flex-end",
+    },
+    input: {
+      textAlign: 'left',
+      backgroundColor: "#F6F6F6",
+      borderColor: "#E8E8E8",
+      borderRadius: 8,
+       height: 40,
+      margin: 12,
+      borderWidth: 1,
+      paddingLeft: 16,
+    },
+    btn: {
+      backgroundColor: "#FF6C00",
+      height:51,
+      borderRadius:100,
+      marginTop:43,
+      justifyContent:"center",
+      alignItems:"center",
+    },
+    btnTitle:{
+      color: "#FFFFFF",
+      fontSize:16,}
+  },);
+  
+    
