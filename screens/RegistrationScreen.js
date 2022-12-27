52a11424bb5985d729dab4dd24e642697d92c675
Alona-Liubchenko <aylin888@rambler.ru>
2022-12-27 18:41:09 +0200

    p5

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,67 +1,148 @@
-import React , {useState}from 'react';
-import { StyleSheet, View, ImageBackground, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
+import React, { useState } from "react";
+import * as Font from 'expo-font';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-const InitialState = {
-    login: "",
-     email:"",
-     password:"",
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+export default function RegistrationScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState)
+  };
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <Viev>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/photoBG.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : ""}
+          >
+            <View
+              style={{ ...styles.form, marginBottom: isShowKeyboard ? 32 : 78 }}
+            >
+              <Viev style={styles.header}>
+                <Text style={styles.hederTitle}>Registration</Text>
+              </Viev>
+              <TextInput
+                style={styles.input}
+                placeholder="Login"
+                value={state.login}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                }}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, login: value }))
+                }
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={state.email}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                }}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, email: value }))
+                }
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                value={state.password}
+                secureTextEntry={true}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                }}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, password: value }))
+                }
+              />
+              <View>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.btn}
+                  onPress={keyboardHide}
+                >
+                  <Text style={styles.btnTitle}>SING UP</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </Viev>
+    </TouchableWithoutFeedback>
+  );
 }
-export default function RegistrationScreen(){
-    return(<Viev><ImageBackground style={styles.image} source={require('./assets/images/photoBG.jpg')}>
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : ""}>
-    <View style={styles.forma}>
-      <Text>Registration</Text>
-      <TextInput style={ styles.input} placeholder="Login"/>
-      <TextInput style={ styles.input} placeholder="Email"/>
-      <TextInput style={ styles.input} placeholder="Password" secureTextEntry={true}/>
-      <View>
-        <TouchableOpacity style={styles.btn}><Text style={styles.btnTitle}>SING UP</Text></TouchableOpacity>
-      </View>
-    </View>
-    </KeyboardAvoidingView>
-  </ImageBackground></Viev>)
-}
-
 
 const styles = StyleSheet.create({
-    // container: {
-    //   flex: 1,
-    //   backgroundColor: '#fff',
-    //   // alignItems: 'center',
-    //   // justifyContent: 'center',
-    // },
-    forma: {
-      backgroundColor: '#FFFFFF',
-      borderRadius: 25,
-      // width: 375,
-      height:549,
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   // alignItems: 'center',
+  //   // justifyContent: 'center',
+  // },
+  form: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 25,
+    // width: 375,
+    height: 549,
+    marginBottom: 32,
   },
-    image: {
-      flex: 1,
-      resizeMode: "cover",
-      justifyContent: "flex-end",
-    },
-    input: {
-      textAlign: 'left',
-      backgroundColor: "#F6F6F6",
-      borderColor: "#E8E8E8",
-      borderRadius: 8,
-       height: 40,
-      margin: 12,
-      borderWidth: 1,
-      paddingLeft: 16,
-    },
-    btn: {
-      backgroundColor: "#FF6C00",
-      height:51,
-      borderRadius:100,
-      marginTop:43,
-      justifyContent:"center",
-      alignItems:"center",
-    },
-    btnTitle:{
-      color: "#FFFFFF",
-      fontSize:16,}
-  },);
-  
-    
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  header: {},
+  hederTitle: {
+    fontFamily: "Roboto",
+    fontStyle: normal,
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: center,
+    letterSpacing: 0.01,
+  },
+  input: {
+    textAlign: "left",
+    backgroundColor: "#F6F6F6",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    paddingLeft: 16,
+  },
+  btn: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    marginTop: 43,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
